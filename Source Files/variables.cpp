38a7b2b2e3a6ad9c45c61f87b43e28070c0280c5
Lisa <llvvss@icloud.com>
2024-03-07 10:23:14 -0700

    first commit

---
+++ Source Files/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float annualSalary = 50000.99;
+    float monthlySalary = annualSalary / 12;
+
+    cout << "Your monthly salary is " << monthlySalary;
+
+    // system("pause>0");    this command does not work for me. It says "sh: pause: command not found"
+}
